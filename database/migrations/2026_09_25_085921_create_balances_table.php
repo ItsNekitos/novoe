08559c02d72e679250944916970fd53551208b11
ItsNekitos <nik-gusev-07@mail.ru>
2026-09-25 14:49:59 +0500

    smalupdate

--- database/migrations/2026_09_25_085921_create_balances_table.php
+++ database/migrations/2026_09_25_085921_create_balances_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('balances', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->float('balance');
+            $table->float('balance')->default(0.00);
             $table->timestamps();
         });
     }
